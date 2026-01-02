3f76b84178c1a322227b0da7b558af0f84f92cd2
rohan <rohanbangera3108@gmail.com>
2026-01-02 20:04:11 +0530

    Adding new files

---
+++ src/test/java/com/rohan/JobApp/JobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohan.JobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
